353a5eb2f2006926ab21418a3fc670cd03c5e85b
Fortune Precious <fortuneprecious17@gmail.com>
2024-03-08 22:13:48 +0100

    imoroved user log api

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.post('/api/users', (req, res) => {
   hash.update(username);
   const id = hash.digest('hex').substr(0, 8);
 
-  const user = ({username: username, _id: id});
+  const user = ({ username: username, _id: id });
   users.push(user);
-  
+
   res.json(user);
 });
 
@@ -37,36 +37,39 @@ app.get('/api/users', (req, res) => {
 // Route to handle POST request submission and retrieval of a user object
 app.post('/api/users/:_id/exercises', (req, res) => {
   const description = req.body.description;
-  const duration = req.body.duration;
+  const duration = Number(req.body.duration);
   const date = req.body.date;
+  let dateObject;
+  if (!date) {
+    dateObject = new Date();
+  } else {
+    dateObject = new Date(date);
+  }
   const _id = req.params._id;
-  const dateObject = new Date(date);
-  const dateStringFormat = dateObject.toString();
+  // const options = { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' };
+  const dateStringFormat = dateObject.toDateString();
 
-  const log = {dateStringFormat, duration, description};
-  let counter;
+  const log = { description, duration, date: dateStringFormat };
   let username;
 
   users.forEach(user => {
     // Check if the current object has the search value
-  if (Object.values(user).includes(_id)) {
-    // If _id is found, push data into the log array
-    username = user.username;
-    if (!user.log || !Array.isArray(user.log)) {
-      // If log doesn't exist or is not an array, initialize it as an empty array
-      user.log = [log];
-      user.count = 1;
-      counter = user.count;
-    } else {
-      // Push data into the k2 array
-      user.log.push(log);
-      user.count = user.count + 1;
-      counter = user.count;
+    if (Object.values(user).includes(_id)) {
+      // If _id is found, push data into the log array
+      username = user.username;
+      if (!user.log || !Array.isArray(user.log)) {
+        // If log doesn't exist or is not an array, initialize it as an empty array
+        user.count = 1;
+        user.log = [log];
+      } else {
+        // Push data into the log array
+        user.count = user.count + 1;
+        user.log.push(log);
+      }
     }
-  }
   });
 
-  res.json({_id: _id, username: username, date: dateStringFormat, duration: duration, description: description});
+  res.json({ _id: _id, username: username, date: dateStringFormat, duration: duration, description: description });
 })
 
 // Route to handle GET request to retrieve users' exercise logs
@@ -74,13 +77,44 @@ app.get('/api/users/:_id/logs', (req, res) => {
   // Extract parameters from URL
   const _id = req.params._id;
   const { from, to, limit } = req.query;
+  let newLog = [];
 
-  // Function to find a user by _id
-  const foundUser = users.find(user => {
-    return user._id == _id;
+  if ((!from || !to) && !limit) {
+    // Function to find a user by _id
+    const foundUser = users.find(user => {
+      return user._id == _id;
+    });
+
+    res.json(foundUser);
+  }
+
+  const userData = users.find(user => {
+    return user._id === _id;
   });
 
-  res.json(foundUser);
+  if ('log' in userData) {
+    const allLogs = userData['log'];
+    let date1Object = new Date(from);
+    const startDate = date1Object.toDateString();
+    let date2Object = new Date(to);
+    const endDate = date2Object.toDateString();
+
+    allLogs.forEach(log => {
+      if(startDate >= log.date && endDate <= log.date){
+        newLog.push(log);
+      }
+    })
+
+    
+  } else {
+    res.json({error: "No log data saved!"});
+  }
+
+  const username = userData.username;
+  const userid = userData._id;
+  const count = userData.count;
+
+  res.json({_id: userid, username: username, count: count, log: newLog});
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
